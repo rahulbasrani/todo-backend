a419953d1c10fd87e15e4e4fe80de6ac2ad672d3
ravi-basrani <rahulbasranikhairthal@gmail.com>
2021-03-24 12:55:50 +0530

    update authjs

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,22 +1,22 @@
-import bcryptjs from 'bcryptjs';
-import config from 'config';
-import jsonwebtoken from 'jsonwebtoken';
+import bcryptjs from "bcryptjs";
+import config from "config";
+import jsonwebtoken from "jsonwebtoken";
 
-import { JwtPayload } from '@typings';
+import { JwtPayload } from "@typings";
 
 export const comparePassword = (
   inputPassword: string,
-  accountPassword: string,
+  accountPassword: string
 ): Promise<boolean> => {
   return bcryptjs.compare(inputPassword, accountPassword);
 };
 
 export const createAccessToken = (payload: JwtPayload): string => {
-  return jsonwebtoken.sign(payload, config.get('jwt.key'), { expiresIn: '1d' });
+  return jsonwebtoken.sign(payload, config.get("jwt.key"), { expiresIn: "1d" });
 };
 
 export const decodeAccessToken = (token: string): JwtPayload => {
-  return jsonwebtoken.verify(token, config.get('jwt.key')) as JwtPayload;
+  return jsonwebtoken.verify(token, config.get("jwt.key")) as JwtPayload;
 };
 
 export const encryptPassword = (password: string): Promise<string> => {
